27f2e2d3f5cbc62c49a965bb18fe531fef3414a2
clair <clair.wang102@gmail.com>
2025-08-18 11:39:15 -0400

    finished the first step

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # zoo_ducks
+## All facts are taken from Wikipedia. https://en.wikipedia.org/wiki/Duck
+## Made for The Zoo 
---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+    import { base } from "$app/paths";
+    import { onMount } from "svelte";
+    let loaded = $state(false); 
+
+    let facts = $state([
+            {
+                "title": "Loading...",
+                "description": "",
+                "icon": "",
+                "id": 0 
+            }
+        ]);
+    
+    let currentFact = $state(1);
+    let totalFacts = $state(0);
+
+    let fact_desc = $derived(facts[currentFact-1].description);
+    let fact_title = $derived(facts[currentFact-1].title);
+
+    onMount (function() {
+        fetch(`${ base }/facts.json`)
+        .then(response => response.json())
+        .then(data => {
+            facts.set(data);
+            loaded = true;
+            totalFacts = facts.length;
+        });
+    })
+
+
+</script>
+
+<svelte:head>
+
+</svelte:head>
+
+<!-- html -->
+<main>
+
+</main>
+
+
+<!-- css -->
+<style>
+
+</style>
