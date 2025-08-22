cd3fc641d2fbe3a724b2dcb126f48d38a5b37382
clair <clair.wang102@gmail.com>
2025-08-21 22:52:10 -0400

    added css but it's not working on my localhost??

--- my-app/src/routes/secret-page/+page.svelte
+++ my-app/src/routes/secret-page/+page.svelte
@@ -136,13 +136,22 @@
 
     .duck-image{
         position: absolute;
+
+        top:0;
+        left:0;
+        width:100%;
+        height:100%;
+
+        object-fit:contain;
+        border-radius: 1000px;
+        transition: transform 0.5s;
     }
 
     .duck-face{
         top:0%;
         left: 0%;
         width: 100%;
-        height: 100%
+        height: 100%;
     }
 
     .duck-message-bubble{
