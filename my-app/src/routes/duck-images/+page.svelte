56a4ed698f1618d344de49541b66036afbdae2e8
clair <clair.wang102@gmail.com>
2025-08-22 23:11:21 -0400

    finished styling the thing, but i don't have the images yet so its bugging

--- my-app/src/routes/duck-images/+page.svelte
+++ my-app/src/routes/duck-images/+page.svelte
@@ -41,9 +41,78 @@
         {/each}
     </div>
     <div class = "back-button-container">
-        <a href = "/"></a> Go back to the main page </a>
+        <a href = "/">Go back to the main page </a>
     </div>
 </main>
 
 <style>
+    :root{
+        --bg-color: #ebe4dc;
+        --card-bg: #fffefd;
+        --text-color: #3f2406;
+        --accent-color: #eec16f;
+    }
+
+    main{
+        min-height: 100vh;
+        background-color: var(--bg-color);
+        padding: 2rem;
+        text-align: center;
+        font-family: 'Google Sans Code', monospace;
+    }
+
+    h1{
+        color: var(--accent-color);
+        font-size: 3rem;
+        margin-bottom: 2rem;
+    }
+
+    .gallery{
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+        gap: 1.6rem;
+        max-width: 1000px;
+        margin: 1rem auto;
+    }
+
+    .image-container{
+        background-color: var(--card-bg);
+        border-radius: 12px;
+        padding: 1rem;
+    }
+
+    .image-container:hover{
+        transform: scale(1.05);
+        transform: translateY(-5px);
+    }
+
+    .image-container img{
+        width: 100%;
+        height: auto;
+        border-radius: 8px;
+    }
+
+    .image-container p{
+        margin-top: 1rem;
+        color: var(--text-color);
+        font-size: 1rem;
+    }
+
+    .back-button-container{
+        margin-top: 3rem;
+    }
+
+    .back-button-container a{
+        background-color: #68735b;
+        color: white;
+        text-decoration: none;
+        padding: 12px 24px;
+        border-radius: 10px;
+    }
+
+    .back-button-container a:hover{
+        background-color: #4c5e40;
+        transform: scale(1.04);
+    }
+
 </style>
